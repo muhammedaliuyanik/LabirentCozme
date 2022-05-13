a8b7d403762decd27373c09f5ada73f33b460ee6
tragonez <muhammedaliuyank16@gmail.com>
2022-05-14 00:15:15 +0300

    update

--- Move.cs
+++ Move.cs
@@ -8,7 +8,23 @@ namespace LabirentCozme
     {
         public static void LabyrinthMove(string[,] matrix)
         {
-            
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 1; j < 63; j++)
+                {
+                    string temp = matrix[i, j];
+
+                    if (temp != "1" || temp != "-")
+                    {
+                        matrix[i, j] = "-";
+                        while (j == 0)
+                        {
+                            j--;
+                        }
+                    }   
+                }
+            }
+            PrintMatrix.Show(matrix);
         }
     }
 }
